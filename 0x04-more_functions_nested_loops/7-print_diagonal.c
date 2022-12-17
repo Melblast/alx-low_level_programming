a4061bb5d9ff484c7130b9315bf9f22de1d49d97
melblast <chideramelugha@gmail.com>
2022-12-16 19:02:38 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -30,15 +30,13 @@ void print_diagonal(int n)
 			{
 				for (j = 0; j < i; j++)
 
-																					{
-
+																				{
 																					_putchar(' ');
-
-																					}
+																				}
 
 																					_putchar(92);
 
-																					_putchar('\n');
+				_putchar('\n');
 																				}
 		}
 
