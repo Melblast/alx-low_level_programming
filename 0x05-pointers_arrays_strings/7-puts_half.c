829a6a14c00c9d60a6491fb42a3e40ff2b9efe59
melblast <chideramelugha@gmail.com>
2022-12-19 19:27:16 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@
 void puts_half(char *str)
 {
 	int count = 0, i;
-
 	while (count >= 0)
+	
 	{
 		if (str[count] == '\0')
 			break;
 		count++;
 	}
+	
 	if (count % 2 == 1)
 		i = count / 2;
 	else
@@ -23,5 +24,5 @@ void puts_half(char *str)
 	
 	for (i++; i < count; i++)
 		_putchar(str[i]);
-_putchar('\n');
+	_putchar('\n');
 }
